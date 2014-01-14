0dad534b1ef2c547ccabc360418168e89be04df3
Mike Haney <txmikester@gmail.com>
2014-01-14 14:31:06 -0600

    first version

---
+++ src/leiningen/new/lt_plugin.clj
@@ -0,0 +1,17 @@
+(ns leiningen.new.lt-plugin
+  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+            [leiningen.core.main :as main]))
+
+(def render (renderer "lt-plugin"))
+
+(defn lt-plugin
+  "FIXME: write documentation"
+  [name]
+  (let [data {:name name
+              :sanitized (name-to-path name)}]
+    (main/info "Generating fresh 'lein new' lt-plugin project.")
+    (->files data
+             ["src/lt/plugins/{{sanitized}}.clj" (render "plug.clj" data)]
+             ["{{sanitized}}.behaviors" (render "behaviors" data)]
+             ["project.clj" (render "project" data)]
+             ["plugin.edn" (render "edn" data)])))
---
+++ src/leiningen/new/lt_plugin/plug.clj
@@ -0,0 +1,29 @@
+(ns lt.plugins.{{sanitized}}
+  (:require [lt.object :as object]
+            [lt.objs.tabs :as tabs]
+            [lt.objs.command :as cmd])
+  (:require-macros [lt.macros :refer [defui behavior]]))
+
+(defui hello-panel [this]
+  [:h1 "Hello from {{name}}"])
+
+(object/object* ::{{name}}.hello
+                :tags [:{{name}}.hello]
+                :name "{{name}}"
+                :init (fn [this]
+                        (hello-panel this)))
+
+(behavior ::on-close-destroy
+          :triggers #{:close}
+          :reaction (fn [this]
+                      (when-let [ts (:lt.objs.tabs/tabset @this)]
+                        (when (= (count (:objs @ts)) 1)
+                          (tabs/rem-tabset ts)))
+                      (object/raise this :destroy)))
+
+(def hello (object/create ::{{name}}.hello))
+
+(cmd/command {:command ::say-hello
+              :desc "{{name}}: Say Hello"
+              :exec (fn []
+                      (tabs/add-or-focus! hello))})
